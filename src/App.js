cb66bf1e57061793801394bd0adefda5e9d651f9
yamabico_dev <38546765+yamabicodev@users.noreply.github.com>
2022-01-02 11:42:06 +0900

    :+1:update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import { SecondaryButton } from "./components/atoms/buttons/SecondaryButton";
-import { PrimaryButton } from "./components/atoms/buttons/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import "./styles.css";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/UserCard";
 
+const user = {
+  name: "yamabico",
+  image: "https://source.unsplash.com/EdJZqsZpZYg",
+  email: "test@gmail.com",
+  phone: "090-1234-5678",
+  company: {
+    name: "hogefuga"
+  },
+  website: "https://hogefuga.com"
+};
 export default function App() {
   return (
     <div className="App">
       <PrimaryButton>OK</PrimaryButton>
       <SecondaryButton>NG</SecondaryButton>
+      <br />
+      <SearchInput></SearchInput>
+      <UserCard user={user} />
     </div>
   );
 }
